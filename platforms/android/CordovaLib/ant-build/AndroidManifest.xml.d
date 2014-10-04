a759d746f246f56e8fcbfc5eeacdf94df6aa457b
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-10-04 09:22:54 +0800

    Ignore build files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/QuickReads-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/ant-build/QuickReads-debug-unaligned.apk : \
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/ant-build/QuickReads.ap_ \
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/ant-build/classes.dex \
-/home/neilmarion/DEV/quickreads2/quickreads/platforms/android/CordovaLib/ant-build/classes.jar \
