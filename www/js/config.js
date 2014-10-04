d8da56f6d1dcd29f0f874320a48b600e0d2cf310
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-10-04 09:35:45 +0800

    Added js files

---
+++ www/js/config.js
@@ -0,0 +1,13 @@
+$conf = {
+  host: "https://quickreads.info",
+  downloadHost: "http://collector.neilmarion.com",
+  app_name: "HumansOfNewYork",
+  album_id: "4",
+  download_folder: "HumansOfNewYork",
+  textCacheFilePath: "humansofnewyork.json",
+  photo_key: "small_photo",
+  small_photo: "small_photo",
+  big_photo: "photo",
+  show_text: true,
+  sender_id: "729983657507"
+}
